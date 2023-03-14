105d550e2f00c886d0d52b174daf54faaef85d5f
dougreichard <doug.reichard+gitlab@gmail.com>
2023-03-14 15:59:29 -0400

    Initial

---
+++ script.py
@@ -0,0 +1,9 @@
+import sbslibs
+from  sbs_utils.handlerhooks import *
+from sbs_utils.gui import Gui
+from sbs_utils.mast.maststoryscheduler import StoryPage
+class MyStoryPage(StoryPage):
+    story_file = "siege.mast"
+
+Gui.server_start_page_class(MyStoryPage)
+Gui.client_start_page_class(MyStoryPage)
