7fc4c3c7224fd81698209829317db606c5bed96c
dougreichard <doug.reichard+github@gmail.com>
2024-03-17 11:13:21 -0400

    Priority Docking, Better station building

--- common_docking.py
+++ common_docking.py
@@ -1,12 +1,58 @@
 import sbs
-from sbs_utils.procedural.query import to_id, to_blob, object_exists, to_engine_object, to_list, inc_disable_weapons_selection, dec_disable_weapons_selection
+from sbs_utils.procedural.query import to_id, to_object, to_blob, object_exists, to_engine_object, to_list, inc_disable_weapons_selection, dec_disable_weapons_selection
 from sbs_utils.procedural.roles import role
+from sbs_utils.procedural.execution import get_shared_variable, task_cancel, task_schedule
 from sbs_utils.procedural.inventory import get_inventory_value, set_inventory_value
+from sbs_utils.procedural.timers import is_timer_finished, set_timer
 from sbs_utils.procedural.space_objects import closest
 from sbs_utils.procedural.grid import grid_objects
 from sbs_utils.tickdispatcher import TickDispatcher
 from internal_damage import grid_restore_damcons, grid_repair_grid_objects
 
+
+__build_times  = {
+    "command": {"build_times": [2, 5, 3, 2]},
+    "civil": {"build_times": [6, 20, 10, 8 ]},
+    "industry": {"build_times": [1, 4, 2, 2 ]},
+    "science": {"build_times": [6, 20, 10, 8 ]},
+    "default": {"build_times": [3, 10, 5, 4 ]}
+}
+
+def get_build_times(id_or_obj):
+    build_times = get_shared_variable("build_times", __build_times)
+    if build_times is None:
+        build_times = __build_times
+    
+    #   HOMING : 0, NUKE : 1, EMP : 2, MINE : 3
+    so = to_object(id_or_obj)
+    if so is not None:
+        artid = so.art_id
+        for k in build_times:
+            if k in artid:
+                return  build_times[k]["build_times"]
+
+    return build_times["default"]["build_times"]
+
+def get_build_time_for(id_or_obj, torp_type):
+    bt = get_build_times(id_or_obj)
+    if torp_type.value < len(bt):
+        return bt[torp_type]  * 60
+
+def build_munition_queue_task(id_or_obj, torp_type):
+    build_task = get_inventory_value(id_or_obj, "build_task")
+    build_type = get_inventory_value(id_or_obj, "build_type")
+
+    if build_type == torp_type:
+        return False
+
+    set_inventory_value(id_or_obj, "build_type", torp_type.value)
+    # if it is running stop it
+    if build_task is not None:
+        task_cancel(build_task)
+    # Start the new work    
+    build_time = get_build_time_for(id_or_obj, torp_type)
+    set_inventory_value(id_or_obj, "build_task", task_schedule("task_station_building", data={"build_time": build_time, "torpedo_build_type": torp_type}))
+    return True
 
 
 
@@ -120,23 +166,38 @@ def player_docking_docked(player_id_or_obj, dock_station):
     dock_station_blob = to_blob(dock_station)
     player_id = to_id(player_id_or_obj)
 
+    refuel_amount = 20
+    load_torp = is_timer_finished(player_id,"priority_docking_torp")
+    if not is_timer_finished(player_id,"priority_docking"):
+        refuel_amount+=20
+        set_timer(player_id,"priority_docking", 2)
+        if load_torp:
+            set_timer(player_id,"priority_docking_torp", 2)
+    else:
+        if load_torp:
+            set_timer(player_id,"priority_docking_torp", 6)
+    
+        
+
     # refuel
     fuel_value = player_blob.get("energy",0)
     if fuel_value < 1000:
-        fuel_value = fuel_value + 20
+        fuel_value = fuel_value + refuel_amount
         player_blob.set("energy", int(fuel_value))
 
 
     # resupply torps
-    for torps in range(sbs.TORPEDO.TORPTYPECOUNT):
-        tLeft = dock_station_blob.get("torpedo_count", torps)
-        if tLeft > 0:
-            torp_max = player_blob.get("torpedo_max", torps)
-            torp_now = player_blob.get("torpedo_count", torps)
-            if torp_now < torp_max:
-                torp_now = torp_now + 1
-                player_blob.set("torpedo_count", torp_now,torps)
-                dock_station_blob.set("torpedo_count", tLeft-1, torps)
+    if load_torp:
+        for torps in range(sbs.TORPEDO.TORPTYPECOUNT):
+            tLeft = dock_station_blob.get("torpedo_count", torps)
+            if tLeft > 0:
+                torp_max = player_blob.get("torpedo_max", torps)
+                torp_now = player_blob.get("torpedo_count", torps)
+                if torp_now < torp_max:
+                    torp_now = torp_now + 1
+                    player_blob.set("torpedo_count", torp_now,torps)
+                    dock_station_blob.set("torpedo_count", tLeft-1, torps)
+
 
 
     #repair shields (more than normal)
