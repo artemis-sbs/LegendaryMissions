2245d41e3d01eb948dfcbba262768cacb40bf367
dougreichard <doug.reichard+github@gmail.com>
2024-03-29 18:59:45 -0400

    NPC fighters switched to standby

--- npc_cag.py
+++ npc_cag.py
@@ -9,7 +9,7 @@ from sbs_utils.procedural.space_objects import target, closest, broad_test_aroun
 from sbs_utils.procedural.roles import role, all_roles, get_race
 from sbs_utils.procedural.science import science_set_scan_data
 from sbs_utils.procedural.spawn import npc_spawn
-from sbs_utils.procedural.links import get_dedicated_link, set_dedicated_link, unlink
+from sbs_utils.procedural.links import get_dedicated_link, set_dedicated_link, unlink, link
 from sbs_utils.procedural.inventory import get_inventory_value, set_inventory_value
 from sbs_utils import faces
 from sbs_utils.vec import Vec3
@@ -73,44 +73,58 @@ class NpcCAG(Agent):
     @label()
     def tick_fighter_launch(self):
         for e in to_object_list(all_roles("npc,carrier")):   # roles npc,carrier
-            if object_exists(e):            
-                blob = e.data_set
-                fighter_count = blob.get("bay_count",0)
-                fighter_count = get_inventory_value(e, "fighters_in_bay", fighter_count)
-                fighter_key = self.get_fighter_key(get_race(e))
-                start_pos = e.pos
+            # If the carrier doesn't exist skip it
+            if not object_exists(e):
+                continue  
 
-                #print(f"tick_fighter_launch: managing {e.name} carrier")
+            blob = e.data_set
+            fighter_count = blob.get("bay_count",0)
+            fighter_count = get_inventory_value(e, "fighters_in_bay", fighter_count)
+            fighter_key = self.get_fighter_key(get_race(e))
+            start_pos = e.pos
 
-                # set up the refit timer for this carrier
-                if not e.has_role("fighter_refit_timer"):
-                    e.add_role("fighter_refit_timer")
-                    set_timer(e, "fighter_refit", seconds = 1)
+            #print(f"tick_fighter_launch: managing {e.name} carrier")
 
-                if is_timer_set(e, "fighter_refit") and is_timer_finished(e, "fighter_refit"):
-                    #find a fighter target within 8k?
-                    target_id = self.find_fighter_target_id(e.id)
-                    if None is not target_id:
+            # set up the refit timer for this carrier
+            if not e.has_role("fighter_refit_timer"):
+                e.add_role("fighter_refit_timer")
+                set_timer(e, "fighter_refit", seconds = 1)
+
+            if is_timer_set(e, "fighter_refit") and is_timer_finished(e, "fighter_refit"):
+                #find a fighter target within 8k?
+                target_id = self.find_fighter_target_id(e.id)
+                # Nothing to target don't launch
+                if target_id is None:
+                    continue
 #                        target_ref = to_object(target_id)
 
-                        #print(f"Launching {fighter_count} fighters")
+                #print(f"Launching {fighter_count} fighters")
+                inactive = self.get_link_set("inactive_fighter_list")
+                if len(inactive) > 0:
+                    for f in inactive:
+                        self.add_link("active_fighter_list", f)
+                        self.remove_link("inactive_fighter_list", f)
+                        craft = to_object(f)
+                        sbs.retrieve_from_standby_list(craft.engine_object)
+                        set_timer(f, "bingo", seconds=120)
+                        target(f, target_id)
+                else:
+                    for g in range(fighter_count):
+                        # launch an npc fighter
+                        nam = f"{e.name} {str(random.randint(0,99)).zfill(2)}"
+                        spawn_data = npc_spawn(start_pos.x, start_pos.y, start_pos.z, nam, "fighter", fighter_key, "behav_npcship")
+                        spawn_id = to_id(spawn_data)
+                        # link them to me
+                        self.add_link("active_fighter_list", spawn_id)
 
-                        for g in range(fighter_count):
-                            # launch an npc fighter
-                            nam = f"{e.name} {str(random.randint(0,99)).zfill(2)}"
-                            spawn_data = npc_spawn(start_pos.x, start_pos.y, start_pos.z, nam, "fighter", fighter_key, "behav_npcship")
-                            spawn_id = to_id(spawn_data)
-                            # link them to me
-                            self.add_link("active_fighter_list", spawn_id)
+                        # set their bingo timer
+                        set_timer(spawn_id, "bingo", seconds=120)
+                        set_dedicated_link(spawn_id, "my_carrier", to_id(e))
+                        # set them on the right path
+                        target(spawn_id, target_id)
 
-                            # set their bingo timer
-                            set_timer(spawn_id, "bingo", seconds=120)
-                            set_dedicated_link(spawn_id, "my_carrier", to_id(e))
-                            # set them on the right path
-                            target(spawn_id, target_id)
-
-                        set_inventory_value(e, "fighters_in_bay", 0)
-                        clear_timer(e, "fighter_refit")
+                set_inventory_value(e, "fighters_in_bay", 0)
+                clear_timer(e, "fighter_refit")
 
         yield AWAIT(delay_sim(seconds=10))
         yield jump(self.tick_fighter_launch)
@@ -121,38 +135,48 @@ class NpcCAG(Agent):
     @label()
     def tick_fighter_manage(self):
         # look at all my active fighters
-        fighter_set = self.get_link_objects("active_fighter_list")
-        for e in fighter_set:
-            if object_exists(e):            
-                #print(f"managing {e.name} fighter")
+        fighter_set = self.get_link_list("active_fighter_list")
+        for fighter_id in fighter_set:
+            craft = to_object(fighter_id)            
+            if craft is None:
+                continue
+            if not object_exists(craft):
+                continue
+            #print(f"managing {e.name} fighter")
 
-                fighter_id = to_id(e)
-                # check the bingo timer
-                if is_timer_finished(fighter_id, "bingo"):
-                    # or get them back to their carrier
-                    carrier = to_object(get_dedicated_link(fighter_id, "my_carrier"))
-                    if None is not carrier:
-                        target(fighter_id, carrier, shoot=False) # going to my fighter
-                        #print(f"bingo fuel")
+            
+            # check the bingo timer
+            if not is_timer_finished(fighter_id, "bingo"):
+                # find a target to direct them to
+                t_id = self.find_fighter_target_id(fighter_id)
+                if None is not t_id:
+                    target(fighter_id, t_id)
+                continue
 
-                        # if they get close enough to their carrier
-                        difference = Vec3(carrier.pos) - Vec3(e.pos)
-                        if difference.length() < 500:
+            # Bingo timer is finished
+            # get them back to their carrier
+            carrier = to_object(get_dedicated_link(fighter_id, "my_carrier"))
+            if carrier is None:
+                continue
+            
+            target(fighter_id, carrier, shoot=False) # going to my fighter
+            # if they get close enough to their carrier
+            difference = Vec3(carrier.pos) - Vec3(craft.pos)
+            if difference.length() < 500:
 
-                            #print(f"recovering {e.name} fighter")
-                            # destroy them and reset the carrier's launch timer
-                            fv = get_inventory_value(carrier, "fighters_in_bay",0)
-                            set_inventory_value(carrier, "fighters_in_bay", fv+1)
+                #print(f"recovering {e.name} fighter")
+                # destroy them and reset the carrier's launch timer
+                fv = get_inventory_value(carrier, "fighters_in_bay",0)
+                set_inventory_value(carrier, "fighters_in_bay", fv+1)
 
-                            set_timer(carrier, "fighter_refit",seconds=60)
-                            unlink(e, "my_carrier", to_id(carrier))
-                            sbs.delete_object(fighter_id)
+                set_timer(carrier, "fighter_refit",seconds=60)
+                carrier.remove_link("active_fighter_list", fighter_id)
+                carrier.add_link( "inactive_fighter_list", fighter_id)
+                # unlink(e, "my_carrier", to_id(carrier))
+                sbs.push_to_standby_list(craft.engine_object)
+                # sbs.delete_object(fighter_id)
 
-                else: #if is_timer_finished(fighter_id, "bingo"):
-                    # find a target to direct them to
-                    t_id = self.find_fighter_target_id(fighter_id)
-                    if None is not t_id:
-                        target(fighter_id, t_id)
+        
 
         yield AWAIT(delay_sim(seconds=2))
         yield jump(self.tick_fighter_manage)
