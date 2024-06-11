8aa5530f64496e25fade39fe9d05c3ae4a0c959d
dougreichard <doug.reichard+github@gmail.com>
2024-06-10 22:46:04 -0400

    comms has RouteLabels

---
+++ comms/give_orders.py
@@ -0,0 +1,13 @@
+from sbs_utils.procedural.inventory import get_inventory_value, set_inventory_value
+from sbs_utils.procedural.comms import comms_navigate
+import sbs
+
+
+#
+# NOTE: These all could be moved to the sbs_utils library
+#
+
+def comms_set_2dview_focus(client_id, focus_id=0):
+    set_inventory_value(client_id, "2dview_alt_ship", focus_id)
+    sbs.assign_client_to_alt_ship(client_id, focus_id)
+    comms_navigate("comms")
