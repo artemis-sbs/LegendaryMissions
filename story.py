ee2d43eddc80769d735f0afaa4cb5eec32cf1b92
dougreichard <doug.reichard+github@gmail.com>
2024-07-06 12:58:26 -0400

    Comms unknown issues, signals, etc.

--- story.py
+++ story.py
@@ -153,7 +153,7 @@ def main_mission_select_template(item):
     gui_row("row-height:2px;background:#ddd;padding:10px,0,10px,3px;")
     gui_row("row-height: 2em;padding:10px,10px,10px,3px;")
     gui_text(f"text:{item.display_name};justify: left;font:gui-3;")
-    gui_row("row-height:5em;padding:10px,0,10px,3px;")
+    gui_row("row-height:15em;padding:10px,0,10px,3px;")
     gui_text(f"text:{text_sanitize(item.desc)};justify: left;color:#999;font:gui-2;")
     
     
