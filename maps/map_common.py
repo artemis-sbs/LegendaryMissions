6c19a4e66c2435c0d7a69e64111ad4afba5943ec
Bassellope <darrin.bright@gmail.com>
2024-07-17 19:47:11 -0400

    ok

--- maps/map_common.py
+++ maps/map_common.py
@@ -615,7 +615,8 @@ def create_siege_fleet(race, fleet_diff, posx, posy, posz, fleet_roles = "Raider
         race = "Kralien"
         siege_fleet = siege_kralien_fleet[fleet_diff][fleet_rand]
 
-    print(f"Create Fleet, Race: {race}, Ships: {siege_fleet}")
+    print(f"Create Fleet, Race: {race}, Ships: {siege_fleet}, Pos: {int(posx)}, {int(posy)}, {int(posz)}")
+
     
     num_ships = len(siege_fleet)
 #    max_carriers = int(max_carriers)
