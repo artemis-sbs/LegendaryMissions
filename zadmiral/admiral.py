fe957ce22d2c687aa6b5233fc40241035dac5e24
dougreichard <doug.reichard+github@gmail.com>
2024-09-16 21:31:35 -0400

    Added move commands

--- zadmiral/admiral.py
+++ zadmiral/admiral.py
@@ -1,5 +1,7 @@
 from sbs_utils.procedural.inventory import  get_inventory_value, set_inventory_value
+from sbs_utils.procedural.query import get_science_selection, get_weapons_selection, to_object
 from sbs_utils.helpers import FrameContext
+from sbs_utils.vec import Vec3
 
 
 def admiral_show_nav_area(ORIGIN_ID, pos, size_delta, text, selection_type, color):
@@ -15,6 +17,8 @@ def admiral_show_nav_area(ORIGIN_ID, pos, size_delta, text, selection_type, colo
         size = 5000
 
     set_inventory_value(ORIGIN_ID, f"ADMIRAL_{selection_type}_SIZE", size)
+    set_inventory_value(ORIGIN_ID, f"ADMIRAL_{selection_type}_x", x)
+    set_inventory_value(ORIGIN_ID, f"ADMIRAL_{selection_type}_y", y)
     
     nav_id = get_inventory_value(ORIGIN_ID, f"ADMIRAL_{selection_type}_SELECT_ID", None)
     if nav_id:
@@ -26,6 +30,25 @@ def admiral_show_nav_area(ORIGIN_ID, pos, size_delta, text, selection_type, colo
     nav.visibleToShip = ORIGIN_ID
     set_inventory_value(ORIGIN_ID, f"ADMIRAL_{selection_type}_SELECT_ID", nav_id)
 
+def admiral_get_pos(ORIGIN_ID, selection_type):
+    x = get_inventory_value(ORIGIN_ID, f"ADMIRAL_{selection_type}_x", 0)
+    y = get_inventory_value(ORIGIN_ID, f"ADMIRAL_{selection_type}_y", 0)
+
+    _other = 0
+    if selection_type == "rmb":
+        _other = get_weapons_selection(ORIGIN_ID)
+    elif selection_type == "lmb":
+        _other = get_science_selection(ORIGIN_ID)
+
+    if _other==0:
+        return Vec3(x,0,y)
+    _obj = to_object(_other)
+    if _obj is None:
+        return Vec3(x,0,y)
+    return _obj.pos
+    
+
+    
 
 from sbs_utils.procedural.gui import gui_row, gui_icon, gui_text
 
