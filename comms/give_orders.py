fd5d9453b4111d78e578e851eb526977a629d154
dougreichard <doug.reichard+github@gmail.com>
2025-03-05 18:36:32 -0500

    New Give Orders via brain+objectives

--- comms/give_orders.py
+++ comms/give_orders.py
@@ -11,9 +11,9 @@ def comms_set_2dview_focus(client_id, focus_id=0):
     if focus_id is None:
         return
     follow = get_inventory_value(client_id, "2d_follow")
+    set_inventory_value(client_id, "2dview_alt_ship", focus_id)
     if not follow:
         focus_id = 0
-    set_inventory_value(client_id, "2dview_alt_ship", focus_id)
     sbs.assign_client_to_alt_ship(client_id, focus_id)
 
     #comms_navigate("comms")
