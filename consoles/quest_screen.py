101a3f1d1c2a329dc47f8c32ec07606df7bd32d8
dougreichard <doug.reichard+github@gmail.com>
2026-01-14 22:39:18 -0500

    more quest test data

--- consoles/quest_screen.py
+++ consoles/quest_screen.py
@@ -1,4 +1,4 @@
-from sbs_utils.procedural.gui import gui_row, gui_text, gui_hole
+from sbs_utils.procedural.gui import gui_row, gui_text, gui_icon, gui_blank
 from sbs_utils.procedural.quest import quest_agent_quests, QuestState, quest_add_yaml
 from sbs_utils.helpers import FrameContext
 from sbs_utils.procedural.gui.listbox import gui_list_box_header, gui_list_box_is_header
@@ -10,21 +10,36 @@ from sbs_utils.agent import Agent
 def quest_item(item):
     
     if not gui_list_box_is_header(item):
-        gui_row("row-height: 1em;padding:5px,0,5px,0;")
+        gui_row("row-height: 1.5em;padding:5px,0,5px,0;")
         id = item.get("id")
-        indent = id.count("/") * 15
-        
+        indent = item.get("indent",0) #id.count("/")
+        if indent:
+            gui_blank(1,f"col-width:{indent*25}px")
 
-        display_text = "  "*indent + item.get("display_text")
-        gui_text(f"$text:{display_text};justify: left;",f"padding:{indent}px,0,0.1em,0;")
-    else:
-        gui_row("row-height: 1em;padding:5px,0,5px,0;")
-        if not item.collapse:
-            label = f"- {item.label}"
-            gui_text(f"$text:{label};justify: left;color:#02FF;", "background: #FFFC")
+        
+        if item.state == QuestState.COMPLETE:
+            gui_icon("icon_index:101;color:#151;", "padding:0,0,5px,0;")
         else:
-            label = f"+ {item.label}"
-            gui_text(f"$text:{label};justify: left;color:#FFF;", "background: #0173")
+            gui_icon("icon_index:121;color:#eee;", "padding:0,0,5px,0;")
+
+        display_text = item.get("display_text")
+        if len(item.children)>0:
+            display_text = "Overview"
+        gui_text(f"$text:{display_text};justify: left;draw_layer:1000;",f"padding:{indent}px,0,0.1em,1em;")
+    else:
+
+        gui_row("row-height: 1.5em;padding:5px,0,5px,0;")
+        if item.indent:
+            gui_blank(1,f"col-width:{item.indent*25}px")
+
+        if not item.collapse:
+            gui_icon("icon_index:155;color:#000;", "padding:0,0,5px,0;background: #FFFC;")
+            label = f"{item.label}"
+            gui_text(f"$text:{label};justify: left;color:#222;", "background: #FFFC")
+        else:
+            gui_icon("icon_index:154;color:#eee;", "padding:0,0,5px,0;background: #0173;")
+            label = f"{item.label}"
+            gui_text(f"$text:{label};justify: left;color:#eee;", "background: #0173")
         return
     
 def quest_title():
@@ -46,23 +61,31 @@ def quest_flatten_list():
 
     ret = []
 
-    def append_quests(quests, header=None):
+    def append_quests(quests, header=None, indent=0):
         active = []
         idle = []
         completed = []
         
         if len(quests)>0:
             if header is not None:
-                active.append(gui_list_box_header(header))
+                active.append(gui_list_box_header(header,False,indent))
 
             for q in quests.values():
+                q.indent = indent
                 state = q.get("state", QuestState.IDLE)
                 if  state == QuestState.ACTIVE:
                     active.append(q)
                     active.extend(append_quests(q.children))
                 if  state == QuestState.IDLE:
-                    idle.append(q)
-                    idle.extend(append_quests(q.children))
+                    c = append_quests(q.children, q.display_text, indent+1)
+                    if len(c)>=2:
+                        q.indent = indent+1
+                        c.insert(1,q)
+                        idle.extend(c)
+                    else:
+                        idle.append(q)
+                    
+                    
                 if state == QuestState.COMPLETE:
                     completed.append(q)
                     completed.extend(append_quests(q.children))
@@ -103,12 +126,19 @@ quest2:
         Two
         Three
 
+        ## Example sub image
+
+        ![](image://test2?scale=0.5&fill=center)
+
+    
+
         ## Ship
         
-        ![](ship://tsn_light_cruiser?height=200&align=center)
+        ![](ship://tsn_light_cruiser?height=100&align=center)
 
+        ## Face
 
-        ![](face://ter #964b00 8 1;ter #968b00 3 0;ter #968b00 4 0;ter #968b00 5 2;ter #fff 3 5;ter #964b00 8 4;?align=right)
+        ![](face://ter #964b00 8 1;ter #968b00 3 0;ter #968b00 4 0;ter #968b00 5 2;ter #fff 3 5;ter #964b00 8 4;?align=left)
         
 
         $h3 This is H3
@@ -130,10 +160,6 @@ quest2:
 
         # Heading one
 
-        ## Heading 2
-
-        ![](image://test2?scale=0.5&fill=center)
-
         ### Heading 3
         
         1. This
@@ -153,6 +179,7 @@ kills:
     children:
         kills25:
             display_text: kill 25
+            state: COMPLETE
         kills50:
             display_text: kill 50
         kills100:
