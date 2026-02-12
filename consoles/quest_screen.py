ab8c738c5cec657734b11c19da3e268febcc4c2f
dougreichard <doug.reichard+github@gmail.com>
2026-02-12 00:06:43 -0500

    Help docs

--- consoles/quest_screen.py
+++ consoles/quest_screen.py
@@ -12,12 +12,13 @@ def quest_item(item):
     
     if not gui_list_box_is_header(item):
         gui_row("row-height: 1.5em;padding:8px,0,0,0;")
-        if item.state == QuestState.COMPLETE:
-            gui_icon("icon_index:101;color:#151;", "padding:0,0,5px,0;")
-        elif item.state == QuestState.FAILED:
-            gui_icon("icon_index:101;color:#a22;", "padding:0,0,5px,0;")
-        else:
-            gui_icon("icon_index:121;color:#eee;", "padding:0,0,5px,0;")
+        if item.get("state"):
+            if item.state == QuestState.COMPLETE:
+                gui_icon("icon_index:101;color:#151;", "padding:0,0,5px,0;")
+            elif item.state == QuestState.FAILED:
+                gui_icon("icon_index:101;color:#a22;", "padding:0,0,5px,0;")
+            else:
+                gui_icon("icon_index:121;color:#eee;", "padding:0,0,5px,0;")
         display_text = item.get("display_text")
         gui_text(f"$text:{display_text};justify: left;draw_layer:1000;","padding:5px,6px,0,0;")
     else:
@@ -25,23 +26,27 @@ def quest_item(item):
 
         data = item.data
         if data is not None:
-            if data.state == QuestState.COMPLETE:
-                gui_icon("icon_index:101;color:#151;", "padding:0,0,5px,0;")
-            else:
-                gui_icon("icon_index:121;color:#eee;", "padding:0,0,5px,0;")
-            
+            if not gui_list_box_is_header(item) and item.get("state"):
+                if data.state == QuestState.COMPLETE:
+                    gui_icon("icon_index:101;color:#151;", "padding:0,0,5px,0;")
+                else:
+                    gui_icon("icon_index:121;color:#eee;", "padding:0,0,5px,0;")
+                
         icon_index = 155 if not item.collapse else 154
 
         
         text = gui_text(f"$text:{item.label};justify: left;color:#fff;", "padding:5px,6px,0,0;background: #1578")
-        icon = gui_icon(f"icon_index:{icon_index};color:#fff;", "padding:0,0,5px,0;background: #1578;")
-        if item.selectable:
-            icon.click_text = ""
-            icon.click_tag = item.collapse_tag
-            icon.click_background = "#aaaa"
-            icon.click_color = "black"
-            icon.background_color = "#1576"
-            text.background_color = "#1576"
+        # if item.data is not None and not data.get("root"):
+        #     icon = gui_icon(f"icon_index:{icon_index};color:#fff;", "padding:0,0,5px,0;background: #1578;")
+        if item.data is not None and not data.get("root"):
+            icon = gui_icon(f"icon_index:{icon_index};color:#fff;", "padding:0,0,5px,0;background: #1578;")
+            if item.selectable:
+                icon.click_text = ""
+                icon.click_tag = item.collapse_tag
+                icon.click_background = "#aaaa"
+                icon.click_color = "black"
+                icon.background_color = "#1576"
+                text.background_color = "#1576"
     return
     
 def quest_title():
