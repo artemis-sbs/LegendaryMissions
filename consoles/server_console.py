a34c5b4c3c30ffe192385ca9c659759f400220b5
dougreichard <doug.reichard+github@gmail.com>
2026-04-23 17:23:48 -0400

    Backticks

--- consoles/server_console.py
+++ consoles/server_console.py
@@ -4,9 +4,9 @@ from sbs_utils.procedural.gui import gui_row, gui_text
 def main_mission_select_template(item):
     gui_row("row-height:2px;background:#ddd;padding:10px,0,10px,3px;")
     gui_row("row-height: 2em;padding:10px,10px,10px,3px;")
-    gui_text(f"$text:{item.display_name};justify: left;font:gui-3;")
+    gui_text(f"$text:`{item.display_name}`;justify: left;font:gui-3;")
     gui_row("row-height:15em;padding:10px,0,10px,3px;")
-    gui_text(f"$text:{item.desc};justify: left;color:#999;font:gui-2;")
+    gui_text(f"$text:`{item.desc}`;justify: left;color:#999;font:gui-2;")
     
     
 
